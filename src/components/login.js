f41d944c6d23f85c7b1a976b64cb7f0998e5b294
bbellify <brianpatrickbellinger@gmail.com>
2023-01-10 01:23:18 -0800

    nothing really

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Urbit from "@urbit/http-api";
 
 export function Login() {
   const navigate = useNavigate();
+  const [error, setError] = useState(false);
   const [login, setLogin] = useState({
     ship: "~",
     url: "",
@@ -20,15 +21,22 @@ export function Login() {
 
   async function connect(e) {
     e.preventDefault();
-    console.log("login in button", login);
+    console.log("connect click");
     window.api = await Urbit.authenticate({
-      ship: prepShipName(login.ship).trim(),
-      url: login.url.trim(),
-      code: login.code.trim(),
+      //   ship: prepShipName(login.ship).trim(),
+      //   url: login.url.trim(),
+      //   code: login.code.trim(),
       // lathus-worsem-bortem-padmel
+      ship: "bud",
+      url: "localhost:80",
+      code: "lathus-worsem-bortem-padmel",
       verbose: true,
-    }).then(() => navigate("/secret"));
-    console.log("api", window.api);
+    })
+      .then((res) => {
+        // how to handle this? the res isn't what I want, I want the SSE
+        console.log("res", res);
+      })
+      .catch(() => setError(true));
   }
 
   function prepShipName(ship) {
@@ -45,6 +53,7 @@ export function Login() {
       <p>url where you connect to your ship</p>
       <input name="url" value={login.url} onChange={handleInput} />
       <button onClick={connect}>log in</button>
+      {error && <p style={{ color: "red" }}>something went wrong</p>}
     </>
   );
 }
